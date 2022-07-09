8e120234aab471161462903d3c003c4986e0dd9d
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2022-07-09 23:45:00 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.6.9
+// @version           0.7.0
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -111,6 +111,7 @@ $(function () {
         if (util.exist('#playerWrap #bilibiliPlayer')) {
           const player_offset_top = $('#playerWrap').offset().top
           util.setValue('player_offset_top', player_offset_top)
+          console.log(player_offset_top,offset_top)
           $('html,body').scrollTop(player_offset_top - offset_top)
           
          
@@ -506,6 +507,8 @@ $(function () {
                 main.autoSelectVideoHightestQuality()
                 applyed = true
               }else{
+                $("#viewbox_report").attr("style","height:106px!important")
+                $(".wide-members").attr("style","height: 99px; overflow: hidden; padding: 10px; box-sizing: border-box;margin-top: -18px;")
                 clearInterval(applyChanges)
               }
               // if (
