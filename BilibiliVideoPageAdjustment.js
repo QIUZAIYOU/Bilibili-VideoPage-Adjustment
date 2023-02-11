b633034325a0c3410f6c00b01788628e98e91b95
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-02-11 22:11:12 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.10.7
+// @version           0.10.8
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -1067,12 +1067,7 @@ $(function () {
       if (player_type === "video") {
         player_offset_top = $("#playerWrap").offset().top;
         utils.setValue("player_offset_top", player_offset_top);
-      }
-      if (player_type === "bangumi") {
-        player_offset_top = $("#player_module").offset().top;
-        utils.setValue("player_offset_top", player_offset_top);
-      }
-      $("#comment").unbind('click').on("click",".jump-link.video-time",function(event){
+        $("#comment").unbind('click').on("click",".jump-link.video-time",function(event){
           event.stopPropagation();
           const targetTime = $(this).attr('data-video-time');
           const video = $('#bilibili-player video')[0]
@@ -1082,6 +1077,21 @@ $(function () {
           if (selected_screen_mod !== "web") $("html,body").scrollTop(player_offset_top - offset_top);
           // console.log(player_offset_top - offset_top)
         })
+      }
+      if (player_type === "bangumi") {
+        player_offset_top = $("#player_module").offset().top;
+        utils.setValue("player_offset_top", player_offset_top);
+        $("#comment_module").unbind('click').on("click",".video-seek",function(event){
+          event.stopPropagation();
+          const targetTime = $(this).attr('data-time');
+          const video = $('#bilibili-player video')[0]
+          video.currentTime = targetTime;
+          video.play();
+          if (selected_screen_mod === "web") $("html,body").scrollTop(0);
+          if (selected_screen_mod !== "web") $("html,body").scrollTop(player_offset_top - offset_top);
+          // console.log(player_offset_top - offset_top)
+        })
+      }
     },
     fixStyle() {
       $("body").css("overflow", "unset");
