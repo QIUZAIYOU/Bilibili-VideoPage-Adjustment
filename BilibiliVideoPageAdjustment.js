2cf5f903253a70c8820c92d88966e3a58d0b2045
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-02-11 20:37:03 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.10.6
+// @version           0.10.7
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -1044,14 +1044,6 @@ $(function () {
             }
           });
         }
-        $(".reply-content .video-time").unbind('click').on("click",function(event){
-          event.stopPropagation();
-          const videoTime = $(this).attr('data-video-time');
-          const video = $('#bilibili-player video')[0]
-          video.currentTime = videoTime;
-          video.play();
-          $("html,body").scrollTop(player_offset_top - offset_top);
-        })
       }
       if(selected_screen_mod === "web"){
         $("html,body").scrollTop(0);
@@ -1065,15 +1057,31 @@ $(function () {
             }
           });
         }
-        $(".reply-content .video-time").unbind('click').on("click",function(event){
-          event.stopPropagation();
-          const videoTime = $(this).attr('data-video-time');
-          const video = $('#bilibili-player video')[0]
-          video.currentTime = videoTime;
-          video.play();
-          $("html,body").scrollTop(0);
-        })
       }
+    },
+    jumpVideoTime(){
+      const selected_screen_mod = utils.getValue("selected_screen_mod");
+      const offset_top = utils.getValue("offset_top");
+      const player_type = utils.getValue("player_type");
+      let player_offset_top;
+      if (player_type === "video") {
+        player_offset_top = $("#playerWrap").offset().top;
+        utils.setValue("player_offset_top", player_offset_top);
+      }
+      if (player_type === "bangumi") {
+        player_offset_top = $("#player_module").offset().top;
+        utils.setValue("player_offset_top", player_offset_top);
+      }
+      $("#comment").unbind('click').on("click",".jump-link.video-time",function(event){
+          event.stopPropagation();
+          const targetTime = $(this).attr('data-video-time');
+          const video = $('#bilibili-player video')[0]
+          video.currentTime = targetTime;
+          video.play();
+          if (selected_screen_mod === "web") $("html,body").scrollTop(0);
+          if (selected_screen_mod !== "web") $("html,body").scrollTop(player_offset_top - offset_top);
+          // console.log(player_offset_top - offset_top)
+        })
     },
     fixStyle() {
       $("body").css("overflow", "unset");
@@ -1405,6 +1413,7 @@ $(function () {
       this.getCurrentPlayerTypeAndScreenMod();
       this.showInformation();
       this.autoSelectScreenMod();
+      this.jumpVideoTime()
       this.playerLoadStateWatcher();
       this.isTopWindow() && this.registerMenuCommand();
       utils.historyListener();
