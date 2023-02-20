328456b268db62353091d35d3c6988ef6281bf32
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-02-20 17:22:50 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.11.1
+// @version           0.11.2
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -161,11 +161,6 @@ $(function() {
       history.pushState = addHistoryMethod("pushState");
       history.replaceState = addHistoryMethod("replaceState");
       window.addHistoryListener("history", function() {
-        main.autoLocation();
-      });
-    },
-    checkBrowserHistory() {
-      window.addEventListener('popstate', () => {
         main.autoLocation();
       });
     },
@@ -1066,7 +1061,7 @@ $(function() {
           }
         });
         $("#Top-Offset").change((e) => {
-          utils.setValue("offset_top", e.target.value);
+          utils.setValue("offset_top", Number(e.target.value));
         });
         $("#Click-Player-Auto-Location").change((e) => {
           utils.setValue("click_player_auto_locate", e.target.checked);
@@ -1117,7 +1112,6 @@ $(function() {
       this.playerLoadStateWatcher();
       this.isTopWindow() && this.registerMenuCommand();
       utils.historyListener();
-      utils.checkBrowserHistory();
     }
   };
   main.init();
