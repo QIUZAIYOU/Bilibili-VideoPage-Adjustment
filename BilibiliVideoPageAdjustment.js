12a86f01edafda4af679fa2a3cdeb1ae8a9f65aa
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2023-06-19 23:22:35 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.19
+// @version           0.20
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -877,17 +877,23 @@ $(() => {
                   </label>
                   <div class="player-adjustment-setting-label screen-mod" style="display: flex;align-items: center;justify-content: space-between;"> 播放器默认模式 <div style="width: 215px;display: flex;align-items: center;justify-content: space-between;">
                       <label class="player-adjustment-setting-label" style="padding-top:0!important;">
+                        <input type="radio" name="Screen-Mod" value="close" ${
+                          getValue('selected_screen_mode') === 'close'
+        ? 'checked'
+        : ''
+        }>关闭</label>
+                      <label class="player-adjustment-setting-label" style="padding-top:0!important;">
                         <input type="radio" name="Screen-Mod" value="wide" ${
                           getValue('selected_screen_mode') === 'wide'
         ? 'checked'
         : ''
-        }>宽屏 </label>
+        }>宽屏</label>
                       <label class="player-adjustment-setting-label" style="padding-top:0!important;">
                         <input type="radio" name="Screen-Mod" value="web" ${
                           getValue('selected_screen_mode') === 'web'
         ? 'checked'
         : ''
-        }> 网页全屏 </label>
+        }>网页全屏</label>
                     </div>
                   </div>
                   <span class="player-adjustment-setting-tips"> -> 若遇到不能自动选择播放器模式可尝试点击重置</span>
@@ -1019,10 +1025,10 @@ $(() => {
               // console.time('播放页调整：切换模式耗时')
               this.watchScreenModeChange()
               await sleep(100)
-              const selectedScreenMode = await this.autoSelectScreenMode()
+              const selectedScreenMode =selected_screen_mode!=='close'? await this.autoSelectScreenMode():'close'
               // console.timeEnd('播放页调整：切换模式耗时')
-              if (selectedScreenMode && selectedScreenMode.done) {
-                logger.info(`屏幕模式｜${selectedScreenMode['mode'].toUpperCase()}｜切换成功`)
+              if ((selectedScreenMode && selectedScreenMode.done) || selectedScreenMode === 'close') {
+                if(selectedScreenMode!=='close') logger.info(`屏幕模式｜${selectedScreenMode['mode'].toUpperCase()}｜切换成功`)
                 this.autoCancelMute()
                 // console.time('播放页调整：选择画质耗时')
                 this.autoSelectVideoHightestQuality()
@@ -1033,6 +1039,7 @@ $(() => {
                 }
                 // console.time('播放页调整：自动定位耗时')
                 this.freezeHeaderAndVideoTitleStyles()
+                await sleep(500)
                 const autoLocationDone = await this.autoLocation()
                 // console.timeEnd('播放页调整：自动定位耗时')
                 if (auto_locate && autoLocationDone) {
