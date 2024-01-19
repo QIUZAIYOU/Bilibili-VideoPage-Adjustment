050f87887de4944e292168dfe54714ac5a94ee9d
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2024-01-19 17:56:52 +0800

    Update BilibiliVideoPageAdjustment.js

--- BilibiliVideoPageAdjustment.js
+++ BilibiliVideoPageAdjustment.js
@@ -2,7 +2,7 @@
 // @name              哔哩哔哩（bilibili.com）播放页调整
 // @license           GPL-3.0 License
 // @namespace         https://greasyfork.org/zh-CN/scripts/415804-bilibili%E6%92%AD%E6%94%BE%E9%A1%B5%E8%B0%83%E6%95%B4-%E8%87%AA%E7%94%A8
-// @version           0.31
+// @version           0.32
 // @description       1.自动定位到播放器（进入播放页，可自动定位到播放器，可设置偏移量及是否在点击主播放器时定位）；2.可设置是否自动选择最高画质；3.可设置播放器默认模式；
 // @author            QIAN
 // @match             *://*.bilibili.com/video/*
@@ -355,7 +355,7 @@ $(() => {
           }, 7000)
           $video.on('canplaythrough', () => {
             // logger.info("视频资源加载｜成功")
-            let attempts = 10
+            let attempts = 100
             const timer = setInterval(() => {
               const isHidden = $('#bilibili-player .bpx-player-container').attr('data-ctrl-hidden')
               if (isHidden === 'false') {
